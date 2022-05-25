b3e0018506a8f401c06ea29b1bca4c459dc08964
Cefqrn <cefqrn@gmail.com>
2022-05-25 19:59:33 -0400

    added way to limit the dimensions of `img_print_image` and ways to get the dimensions of an `image`

--- src/image.c
+++ src/image.c
@@ -10,9 +10,9 @@ typedef struct image {
     uint8_t (**pixels)[3];
 } image;
 
-void img_print_image(image *im, size_t pixelWidth) {
-    for (size_t y=0; y < im->height; ++y) {
-        for (size_t x=0; x < im->width; ++x) {
+void img_print_image(image *im, size_t pixelWidth, size_t maxWidth, size_t maxHeight) {
+    for (size_t y=0; y < im->height && y < maxHeight; ++y) {
+        for (size_t x=0; x < im->width && x * pixelWidth < maxWidth; ++x) {
             char color[64];
             snprintf(color, 64, "\x1b[48;2;%u;%u;%um", im->pixels[x][y][0], im->pixels[x][y][1], im->pixels[x][y][2]);
             for (size_t i=0; i < pixelWidth; ++i) {
@@ -24,7 +24,7 @@ void img_print_image(image *im, size_t pixelWidth) {
     }
 }
 
-image *img_create_image(uint16_t width, uint16_t height) {
+image *img_create_image(size_t width, size_t height) {
     image *im = malloc(sizeof *im);
     CHECK_ALLOC(im, "image");
 
@@ -42,7 +42,7 @@ image *img_create_image(uint16_t width, uint16_t height) {
     return im;
 }
 
-void img_set_pixel(image *im, uint16_t x, uint16_t y, uint8_t componentIndex, uint8_t componentValue) {
+void img_set_pixel(image *im, size_t x, size_t y, size_t componentIndex, uint8_t componentValue) {
     im->pixels[x][y][componentIndex] = componentValue;
 }
 
@@ -63,7 +63,28 @@ void img_yuv_to_rgb(image *im) {
     }
 }
 
+void img_rgb_to_yuv(image *im) {
+    for (size_t x=0; x < im->width; ++x) {
+        for (size_t y=0; y < im->height; ++y) {
+            uint8_t pixel[3];
+            memcpy(pixel, im->pixels[x][y], 3 * sizeof *pixel);
+
+            im->pixels[x][y][0] = clamp(  0.299 * pixel[0] +  0.587 * pixel[1] +  0.114 * pixel[2]);
+            im->pixels[x][y][1] = clamp(-0.1687 * pixel[0] - 0.3313 * pixel[1] +    0.5 * pixel[2] + 128.0);
+            im->pixels[x][y][2] = clamp(    0.5 * pixel[0] - 0.4187 * pixel[1] - 0.0813 * pixel[2] + 128.0);
+        }
+    }
+}
+
 void img_free_image(image *im) {
     FREE_2D_ARRAY(im->pixels, im->width);
     free(im);
+}
+
+size_t img_get_width(image *im) {
+    return im->width;
+}
+
+size_t img_get_height(image *im) {
+    return im->height;
 }
--- src/image.h
+++ src/image.h
@@ -5,10 +5,13 @@
 
 typedef struct image image;
 
-void img_print_image(image *im, size_t pixelWidth);
-image *img_create_image(uint16_t width, uint16_t height);
-void img_set_pixel(image *im, uint16_t x, uint16_t y, uint8_t componentIndex, uint8_t componentValue);
+void img_print_image(image *im, size_t pixelWidth, size_t maxWidth, size_t maxHeight);
+image *img_create_image(size_t width, size_t height);
+void img_set_pixel(image *im, size_t x, size_t y, size_t componentIndex, uint8_t componentValue);
 void img_yuv_to_rgb(image *im);
+void img_rgb_to_yuv(image *im);
 void img_free_image(image *im);
+size_t img_get_width(image *im);
+size_t img_get_height(image *im);
 
 #endif
