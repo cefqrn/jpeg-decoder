6a2847fc95d422ab30a65feb1ee4110327177ec1
Cefqrn <cefqrn@gmail.com>
2022-08-05 18:10:49 -0400

    fixed bug where `stream` doesn't load in the first character when initializing

--- src/bitstream.c
+++ src/bitstream.c
@@ -2,7 +2,6 @@
 #include "macros.h"
 #include "stdio.h"
 #include <stdlib.h>
-#include <string.h>
 
 #define CHAR_WIDTH 8
 
@@ -12,34 +11,40 @@ typedef struct h_bit_stream {
     size_t bitIndex;
 } bit_stream;
 
+static void get_next_char(bit_stream *stream) {
+    stream->bitIndex = 0;
+    
+    // ignore byte stuffing
+    if (stream->c == (char)0xFF) {
+        getc(stream->fp);
+    }
+
+    stream->c = getc(stream->fp);
+    CHECK_FAIL(feof(stream->fp), "Tried to get bit from dry stream.");
+}
+
 bit_stream *str_create_bit_stream(FILE *fp) {
-    bit_stream *str = calloc(1, sizeof *str);
-    CHECK_ALLOC(str, "stream");
+    bit_stream *stream = calloc(1, sizeof *stream);
+    CHECK_ALLOC(stream, "stream");
 
-    str->fp = fp;
-    str->bitIndex = 0;
+    stream->fp = fp;
+    stream->bitIndex = 0;
 
-    return str;
+    // load in first char of the stream
+    get_next_char(stream);
+
+    return stream;
 }
 
 // returns the next bit in stream
 int str_get_bit(bit_stream *stream) {
     // if all of the bits in the current char have been outputted, move to the next char
-    if (stream->bitIndex == CHAR_WIDTH) {
-        stream->bitIndex = 0;
-        
-        char c = getc(stream->fp);
-        
-        // ignore byte stuffing
-        stream->c = stream->c == (char)0xFF ? getc(stream->fp) : c;
-
-        CHECK_FAIL(feof(stream->fp), "Tried to get bit from dry stream.");
-    }
+    if (stream->bitIndex == CHAR_WIDTH) get_next_char(stream);
 
     return (stream->c >> (CHAR_WIDTH - ++stream->bitIndex)) & 0x1;
 }
 
-// returns the next n bits in stream and converts returns the bit array as an integer
+// returns the next n bits in stream and returns the bit array as an integer
 int str_get_bits(bit_stream *stream, int n) {
     int value = 0;
     for (int i=0; i < n; ++i) {
