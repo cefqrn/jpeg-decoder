8d3402be61dfe333f452ce76865b4082420bac7f
Cefqrn <cefqrn@gmail.com>
2023-02-04 15:00:53 -0500

    removed unused macro

--- src/macros.h
+++ src/macros.h
@@ -10,6 +10,4 @@
 #define CHECK_FAIL(cond, ...) if (cond) FAIL(__VA_ARGS__)
 #define CHECK_ALLOC(varName, name) CHECK_FAIL(varName == NULL, "Could not allocate memory for %s.", name)
 
-#define FREE_2D_ARRAY(arrayName, length) for (size_t i=0; i < length; ++i) {free(arrayName[i]);} free(arrayName)
-
 #endif
